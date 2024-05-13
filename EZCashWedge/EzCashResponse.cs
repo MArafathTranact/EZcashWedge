61c3ed7ccf760009636a357069121011485b138b
Mohamed Arafath <arafath@scrapdragon.com>
2024-05-13 15:53:06 -0400

    Adding EZcash Wedge Solution to repo

---
+++ EZCashWedge/EzCashResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZCashWedge
+{
+    public class EzCashResponse
+    {
+        public EzCashResponse()
+        {
+            Success = false;
+            FailureString = string.Empty;
+            Barcode = null;
+            Reprint = false;
+            CardStatus = string.Empty;
+            InitialAmount = 0m;
+            AvailableAmount = 0m;
+            PartialPayPaidAmount = 0m;
+            PartialPayTotal = 0m;
+            PaymentNumber = string.Empty;
+        }
+
+        public bool Success { get; set; }
+
+        public bool Rejected { get; set; }
+
+        public bool Failed { get; set; }
+
+        public bool PartialPay { get; set; }
+
+        public string Barcode { get; set; }
+
+        public string FailureString { get; set; }
+
+        public decimal PartialPayPaidAmount { get; set; }
+
+        public decimal PartialPayTotal { get; set; }
+
+        public bool Reprint { get; set; }
+        public decimal InitialAmount { get; set; }
+        public decimal AvailableAmount { get; set; }
+        public string CardStatus { get; set; }
+        public string PaymentNumber { get; set; }
+        public long TranID { get; set; }
+        public string amount { get; set; }
+
+    }
+}
---
+++ EZCashWedge/YardsTcpListener.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZCashWedge
+{
+
+    public class YardInformation
+    {
+        public int PortNumber { get; set; }
+        public string YardId { get; set; }
+    }
+    public class YardsTcpListener
+    {
+
+        List<YardInformation> yardInformations = new List<YardInformation>();
+        public YardsTcpListener()
+        {
+
+            var yardIdCollection = ConfigurationManager.GetSection("yardIdSection") as NameValueCollection;
+
+            if (yardIdCollection != null && yardIdCollection.AllKeys.Length != 0)
+            {
+                for (int i = 0; i < yardIdCollection.AllKeys.Length; i++)
+                {
+                    try
+                    {
+                        yardInformations.Add(new YardInformation { PortNumber = int.Parse(yardIdCollection.GetKey(i)), YardId = yardIdCollection.GetValues(i).FirstOrDefault() });
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogExceptionWithNoLock($" Exception at Reading YardId/Port Section :", ex);
+                    }
+
+                }
+            }
+            else
+            {
+                Logger.LogWarningWithNoLock($" YardSection Port/YardId is not available in config file to create listener .");
+                return;
+            }
+
+        }
+
+
+
+        List<AsynchronousSocketListener> lstAsyncListener = new List<AsynchronousSocketListener>();
+        public async void CreateListeners()
+        {
+            try
+            {
+                List<Task> tasks = new List<Task>();
+                foreach (var port in yardInformations)
+                {
+                    AsynchronousSocketListener socketListener = new AsynchronousSocketListener(port.PortNumber, port.YardId);
+                    tasks.Add(CreateListenerThread(socketListener));
+                }
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogExceptionWithNoLock($" Exception at YardsTcpListener.CreateListeners", ex);
+            }
+
+
+        }
+
+        public async Task StopYardListeners()
+        {
+            foreach (var item in lstAsyncListener)
+            {
+                try
+                {
+                    if (item != null)
+                        await item.StopListener();
+
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogExceptionWithNoLock($" Exception at YardsTcpListener.StopYardListeners", ex);
+                }
+            }
+
+            lstAsyncListener.Clear();
+            yardInformations.Clear();
+
+        }
+
+
+        private async Task CreateListenerThread(AsynchronousSocketListener socketListener)
+        {
+            try
+            {
+                lstAsyncListener.Add(socketListener);
+                await Task.Run(() => { socketListener.StartListening(); });
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+    }
+}
